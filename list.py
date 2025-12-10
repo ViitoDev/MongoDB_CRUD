d9bcc268bc06ddd0dcf205ef361c48955a23b9b3
ViitoDev <joaovictorpiresdev@gmail.com>
2025-12-10 20:53:16 -0300

    FEAT - Read data of db

---
+++ list.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient()
+mydb = client.dbposts
+mycol = mydb.posts
+
+result = mycol.find()
+
+for r in result:
+    pprint(r)
